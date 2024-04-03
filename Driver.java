60d421e82aa02620c0ccdced46982bbb6fb1022b
DijanU <156262584+DijanU@users.noreply.github.com>
2024-04-02 19:35:13 -0600

    le fini
    ya chambee

--- Driver.java
+++ Driver.java
@@ -1,68 +1,58 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Driver {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         String database = "texto.txt";
 
-        UVGBST <Association<String, String>> tree = new UVGBST<Association<String, String>>();
+        UVGBST<String> tree = new UVGBST<>();
 
-        //variables de setteo
-        boolean run = true;
-        ArrayList<String> sentence = new ArrayList<>();
-        String menu = "=== Translator/Traductor ===\n"+
-        "Ingresa una oracion en ingles y traduciremos todo lo que podamos.";
+        String menu = "=== Translator/Traductor ===\n" +
+                "Ingresa una oracion en ingles y traduciremos todo lo que podamos.";
 
         Scanner scanner = new Scanner(System.in);
 
-                //lector csv
+        // CSV reader
         try (BufferedReader br = new BufferedReader(new FileReader(database))) {
-                String encabezado = br.readLine();
-                String linea;
-                while ((linea = br.readLine()) != null) {
-                    String[] valores = linea.split(",");
-                    
-                    // Extraer los valores del CSV
-                    String english = valores[0];
-                    String espanol = valores[1];
-                    tree.add(new Association<String, String>(english, espanol));
-                System.out.println(tree);
+            String encabezado = br.readLine();
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] valores = linea.split(",");
 
-
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+                String english = valores[0];
+                String espanol = valores[1];
+                tree.add(english, espanol);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println(tree);
+        // Print tree content
+        System.out.println("Contenido del árbol:");
         tree.getRoot().traverseInOrder();
 
-        while(run == true){
-
+        while (true) { // Infinite loop, you may want to add an exit condition
             String outputString = "";
             System.out.println(menu);
             System.out.println("Ingrese tu oracion: ");
             String opcion = scanner.nextLine();
             String[] input = opcion.split(" ");
 
-            for (int i = 0; i < input.length; i++) {
-                if (tree.get(input[i]) == null){
-                    sentence.add("*"+input[i]+"*");
-                }
-                else {
-                    sentence.add(tree.get(input[i]));
+            for (String word : input) {
+                Association<String, String> translatedWord = tree.get(word);
+                if (translatedWord == null) {
+                    outputString += "*" + word + "* ";
+                } else {
+                    outputString += translatedWord + " ";
                 }
             }
 
-            for (int i = 0; i < sentence.size(); i++) {
-                outputString = outputString + sentence.get(i);
-            }
-            
+            // Print the translated sentence
+            System.out.println("Oración traducida:");
+            System.out.println(outputString);
         }
-        
-
     }
 }
+
--- UVGBST.java
+++ UVGBST.java
@@ -2,81 +2,88 @@ public class UVGBST<E extends Comparable<E>> {
 
     public class Node<E extends Comparable<E>>{
         public E value;
+        public E translatedValue; // New attribute for translated value
         public Node leftChild = null;
-        // Cambie a usar <E> para evitar usar cating.
         public Node rightChild = null;
         public Node parent = null;
-        public Node(E cValue){
+        
+        public Node(E cValue, E cTranslatedValue) { // Constructor with two attributes
             this.value = cValue;
+            this.translatedValue = cTranslatedValue;
         }
 
-        public Node(E cValue, Node<E> parent){
+        public Node(E cValue, E cTranslatedValue, Node<E> parent) { // Constructor with two attributes and parent node
             this.value = cValue;
+            this.translatedValue = cTranslatedValue;
             this.parent = parent;
         }
 
-        public String toString(){
+        public String toString() {
             return value.toString();
         }
 
-        public Node<E> addNode(E newVal) {
+        public Node<E> addNode(E newVal, E newTranslatedVal) {
             int compCase = newVal.compareTo(value);
             if (compCase < 0) {
                 if (leftChild == null) {
-                    leftChild = new Node(newVal, this);
+                    leftChild = new Node(newVal, newTranslatedVal, this);
                     return leftChild;
                 } else {
-                    return leftChild.addNode(newVal);
+                    return leftChild.addNode(newVal, newTranslatedVal);
                 }
             }
             if (compCase == 0) {
                 // Update the value
                 value = newVal;
+                translatedValue = newTranslatedVal; // Update the translated value
                 return this;
             }
             if (rightChild == null) {
-                rightChild = new Node(newVal, this);
+                rightChild = new Node(newVal, newTranslatedVal, this);
                 return rightChild;
             }
-            return rightChild.addNode(newVal);
+            return rightChild.addNode(newVal, newTranslatedVal);
         }
 
-        public int nodeHeight(){
+        public int nodeHeight() {
             int currHeight = -1;
-            if (this.leftChild != null){
+            if (this.leftChild != null) {
                 currHeight = this.leftChild.nodeHeight();
             }
-            if (this.rightChild != null){
+            if (this.rightChild != null) {
                 int rightHeight = this.leftChild.nodeHeight();
-                if (rightHeight > currHeight){
+                if (rightHeight > currHeight) {
                     currHeight = rightHeight;
                 }
             }
             return currHeight + 1;
         }
 
-        public int nodeDepth(){
-            if (this.parent == null){
+        public int nodeDepth() {
+            if (this.parent == null) {
                 return 0;
             }
             return 1 + this.parent.nodeDepth();
         }
 
-        public Node<E> getNode(E key){
-            if (this.value.compareTo(key)==0){
+        public Node<E> getNode(E key) {
+            if (this.value.compareTo(key) == 0) {
                 return this;
+            } else if (this.value.compareTo(key) > 0 && this.leftChild != null) {
+                return this.leftChild.getNode(key);
+            } else if (this.value.compareTo(key) < 0 && this.rightChild != null) {
+                return this.rightChild.getNode(key);
+            } else {
+                return null; // Key not found
             }
-            // busque a la izquierda o a la derecha
-            // como se que lado buscar
-            return null;
         }
 
-        public void traverseInOrder(){
-            if (this.leftChild !=null){
+        public void traverseInOrder() {
+            if (this.leftChild != null) {
                 this.leftChild.traverseInOrder();
             }
             System.out.println("--" + this.toString() + "--");
-            if (this.rightChild!=null){
+            if (this.rightChild != null) {
                 this.rightChild.traverseInOrder();
             }
         }
@@ -84,32 +91,36 @@ public class UVGBST<E extends Comparable<E>> {
 
     private Node<E> root;
 
-    public Node<E> getRoot(){
+    public Node<E> getRoot() {
         return root;
     }
 
-    public UVGBST(){
+    public UVGBST() {
         this.root = null;
     }
 
-    public E add(E newVal){
-        if (root == null){
-            root = new Node<E>(newVal);
+    public E add(E newVal, E newTranslatedVal) {
+        if (root == null) {
+            root = new Node<E>(newVal, newTranslatedVal);
             return root.value;
         }
-        return (E) root.addNode(newVal).value;
+        return root.addNode(newVal, newTranslatedVal).value;
     }
 
-    public int height(){
-        if (this.root  == null){
+    public int height() {
+        if (this.root == null) {
             return -1;
         }
         return this.root.nodeHeight();
     }
 
-    public E get(E key){
-
-        return null;
+    public Association<String, String> get(E key) {
+        Node<E> node = this.root.getNode(key);
+        if (node != null) {
+            return new Association<>(node.value.toString(), node.translatedValue.toString());
+        } else {
+            return null; // Key not found
+        }
     }
 
     public String toString() {
@@ -119,19 +130,16 @@ public class UVGBST<E extends Comparable<E>> {
         return "X";
     }
 
-    public String vPrintSubTree(Node<E> x, int depth){
+    public String vPrintSubTree(Node<E> x, int depth) {
         String txt = "";
         txt = " ".repeat(depth);
         txt += "->" + x.value.toString() + "\n";
-        if (x.leftChild!=null){
+        if (x.leftChild != null) {
             txt = vPrintSubTree(x.leftChild, depth + 1) + "\n" + txt;
         }
-        if (x.rightChild!=null){
+        if (x.rightChild != null) {
             txt = txt + "\n" + vPrintSubTree(x.rightChild, depth + 1);
         }
         return txt;
     }
-
-
-
 }
